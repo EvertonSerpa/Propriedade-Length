9bc9e6169e1cf7080a40ef5614d0b78e4eacd380
EvertonSerpa <evertonnascimento.serpa@gmail.com>
2022-01-22 12:04:49 -0300

    propriedade length

---
+++ README.md
@@ -0,0 +1,13 @@
+Usando a propriedade length para;
+
+1 - Obter o tamanho de uma string.
+
+2 - Conhecer o tamanho de um array.
+
+3 - Verificar se uma string está vazia (para validações).
+
+4 - Validar o tamanho de uma senha.
+
+5 - Validação de um CPF.
+
+6 - Interando sobre uma array com o length.
---
+++ length.js
@@ -0,0 +1,56 @@
+/* Exemplo 01 
+
+No exemplo a seguir demonstramos como obter o tamanho de uma string: */
+
+var stringExemplo = "Aprendendo JavaScript";
+var tamanho = stringExemplo.length;
+console.log(tamanho);
+
+/* Exemplo 02 
+
+No exemplo a seguir demonstramos como utilizar a propriedade length quando é preciso conhecer o tamanho de um array: */
+
+var frutas = ["banana", "abacaxi", "abacate", "laranja", "morango", "abacate"];
+console.log(frutas.length);
+
+/* Exemplo 03 
+
+No exemplo a seguir demonstramos como verificar se uma string está vazia, algo que precisa ser validado em formulários 
+e pode prejudicar o funcionamento da aplicação:*/
+
+var stringVazia = "";
+if (stringVazia.length == 0) {
+  console.log("String Vazia");
+}
+
+/* Exemplo 04 
+Também podemos utilizar a propriedade para validar o tamanho de uma senha: */
+
+var senha = "12345";
+if (senha.length < 6) {
+  console.log("A senha precisa ter 6 (seis) caracteres");
+}
+
+/* Exemplo 05 
+
+No exemplo a seguir demonstramos como fazer uma validação simples de CPF: */
+
+
+var cpf = "124.587.748-15"
+
+if(cpf.length == "14") {
+    console.log("CPF com o tamanho correto")
+
+}else {
+    console.log("Verificar campo do CPF")
+}
+
+/* Exemplo 06 
+
+No exemplo a seguir iremos iterar sobre um array utilizando a propriedade length. */
+
+var frutas = ["banana", "abacaxi", "abacate", "laranja", "morango", "abacate"];
+for (var posicao = 0; posicao < frutas.length; posicao++) {
+    console.log(frutas[posicao])
+}
+
